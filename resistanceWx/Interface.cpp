8c6148f5bd44e9c3ab77b6668c3c68285aabd83b
Maria <Maria@DESKTOP-P97Q35F>
2019-04-14 21:10:37 +0300

    Lider-Label

--- resistanceWx/Interface.cpp
+++ resistanceWx/Interface.cpp
@@ -13,9 +13,19 @@
 #include "Player.h"
 #include <iostream>
 
-void change_player(std::string* player_choosen, const sf::String& param) // Команда, чтобы поменять выбранного игрока по нажатию
+void change_player(std::string* player_choosen, Game** gm, const sf::String& param) // Команда, чтобы поменять выбранного игрока по нажатию
 {
+	for (auto p : (*gm)->GetGameAgents()->GetAgents())
+	{
+		if ((param == p->GetPlayer().GetNickName()) && (p->GetIsLider()))
+		{
+			(*player_choosen) = param.toAnsiString();
+			(*player_choosen) += " W";
+			return;
+		}
+	}
 	(*player_choosen) = param.toAnsiString();
+	return;
 }
 
 int main()
@@ -24,34 +34,34 @@ int main()
 
 
 	#pragma region InitRegion
-	char first0[] = "Alex";
-	char sec0[] = "Ф";
-	char code0[] = "Jhon";
+	string first0 = "Alex";
+	string sec0 = "Ф";
+	string code0 = "Alex";
 	int rate0 = 100;
 	int age0 = 48;
 	Player* pl0 = new Player(first0, sec0, code0, rate0, age0);
 	gm->GetGameAgents()->RegistryAgent(pl0);
 
-	char first1[] = "Oleg";
-	char sec1[] = "Ф";
-	char code1[] = "Ivan";
+	string first1 = "Oleg";
+	string sec1 = "Ф";
+	string code1 = "Oleg";
 	int rate1 = 100;
 	int age1 = 45;
 	Player* pl1 = new Player(first1, sec1, code1, rate1, age1);
 	gm->GetGameAgents()->RegistryAgent(pl1);
 
-	char first2[] = "Masha";
-	char sec2[] = "Ф";
-	char code2[] = "Henry";
+	string first2 = "Masha";
+	string sec2 = "Ф";
+	string code2 = "Masha";
 	int rate2 = 100;
 	int age2 = 18;
 	Player* pl2 = new Player(first2, sec2, code2, rate2, age2);
 	gm->GetGameAgents()->RegistryAgent(pl2);
 
 
-	char first3[] = "Anna";
-	char sec3[] = "Ф";
-	char code3[] = "Fred";
+	string first3 = "Anna";
+	string sec3 = "Ф";
+	string code3 = "Anna";
 	int rate3 = 100;
 	int age3 = 15;
 	Player* pl3 = new Player(first3, sec3, code3, rate3, age3);
@@ -61,33 +71,33 @@ int main()
 	else
 		cout << "Игроков достаточно\n";
 
-	char first4[] = "Anastasia";
-	char sec4[] = "Ф";
-	char code4[] = "Dart Vaider";
+	string first4 = "Anastasia";
+	string sec4 = "Ф";
+	string code4 = "Anastasia";
 	int rate4 = 100;
 	int age4 = 65;
 	Player* pl4 = new Player(first4, sec4, code4, rate4, age4);
 	gm->GetGameAgents()->RegistryAgent(pl4);
 
-	char first5[] = "Rita";
-	char sec5[] = "Ф";
-	char code5[] = "Ginny";
+	string first5 = "Rita";
+	string sec5 = "Ф";
+	string code5 = "Rita";
 	int rate5 = 100;
 	int age5 = 18;
 	Player* pl5 = new Player(first5, sec5, code5, rate5, age5);
 	gm->GetGameAgents()->RegistryAgent(pl5);
 
-	char first6[] = "Slava";
-	char sec6[] = "Ф";
-	char code6[] = "Peter";
+	string first6 = "Slava";
+	string sec6 = "Ф";
+	string code6 = "Slava";
 	int rate6 = 100;
 	int age6 = 18;
 	Player* pl6 = new Player(first6, sec6, code6, rate6, age6);
 	gm->GetGameAgents()->RegistryAgent(pl6);
 
-	char first7[] = "Nastya";
-	char sec7[] = "Ф";
-	char code7[] = "Lisa";
+	string first7 = "Nastya";
+	string sec7 = "Ф";
+	string code7 = "Nastya";
 	int rate7 = 100;
 	int age7 = 18;
 	Player* pl7 = new Player(first7, sec7, code7, rate7, age7);
@@ -112,7 +122,7 @@ int main()
 	////players_vector.push_back("Molly");
 	////players_vector.push_back("Gerald");
 
-	//std::string player_choosen = players_vector[0]; //Какого игрока отображать на экране. По умолчанию первого.
+	std::string player_choosen = gm->GetGameAgents()->GetAgents()[0]->GetPlayer().GetNickName() + " W"; //Какого игрока отображать на экране. По умолчанию первого.
 	//// Конец наполнения
 
 	////Загружаем основной интерфейс из файла, после инициализируем все нужные виджеты
@@ -176,7 +186,7 @@ int main()
 		menu->addMenuItem(p->GetPlayer().GetFirstName());//Добавляем менюшку с именами игроков. На саму менюшку кликать нельзя, поэтому добавляем подменю
 	}
 
-	menu->connect("MenuItemClicked", change_player, &(gm->GetGameAgents()->GetAgents()[0]->GetPlayer().GetNickName())); // Команда, чтобы поменять выбранного игрока по нажатию
+	menu->connect("MenuItemClicked", change_player, &player_choosen, &gm); // Команда, чтобы поменять выбранного игрока по нажатию
 	//Далее знаем, от чьего лица сейчас показывается экран. Имя выбранного игрока должно показываться сверху
 	////////////////////////
 
@@ -214,7 +224,7 @@ int main()
 		k++;
 	}
 
-
+	//Начало игрового алгоритма
 
 	while (window.isOpen())
 	{
@@ -228,7 +238,7 @@ int main()
 		}
 
 		////Основной алгоритм////
-		label_curr_player->setText(gm->GetGameAgents()->GetAgents()[0]->GetPlayer().GetNickName());
+		label_curr_player->setText(player_choosen);
 
 
 
